7d476f362fd085b40db8d27f79568d6f17eaadef
Not James <142885453+RebelCoderJames@users.noreply.github.com>
2023-10-24 08:24:52 +0200

    move filepicker to its own file

---
+++ windows-console-notes-editor/windows-console-notes-editor/filepicker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace windows_console_notes_editor
+{
+    internal partial class Program
+    {
+        static string FilePicker()
+        {
+            return "path";
+        }
+    }
+}
