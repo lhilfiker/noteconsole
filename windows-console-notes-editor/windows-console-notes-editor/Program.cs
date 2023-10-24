69c66ffc1ba59ac62045fe5d7284e3a0868151f6
Milan <m.jankovic.inf23@stud.bbbaden.ch>
2023-10-24 09:32:28 +0200

    created welcome screen

--- windows-console-notes-editor/windows-console-notes-editor/Program.cs
+++ windows-console-notes-editor/windows-console-notes-editor/Program.cs
@@ -4,12 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("TEest");
-        }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
 
-        static string FilePicker()
-        {
-            return "path";
+            Console.WriteLine("             _                                 _      ");
+            Console.WriteLine("            | |                               | |     ");
+            Console.WriteLine(" _ __   ___ | |_ ___  ___ ___  _ __  ___  ___ | | ___ ");
+            Console.WriteLine("| '_ \\ / _ \\| __/ _ \\/ __/ _ \\| '_ \\/ __|/ _ \\| |/ _ \\");
+            Console.WriteLine("| | | | (_) | ||  __/ (_| (_) | | | \\__ \\ (_) | |  __/");
+            Console.WriteLine("|_| |_|\\___/ \\__\\___|\\___\\___/|_| |_|___/\\___/|_|\\___|");
+            Console.WriteLine("                                                      ");
+            Console.WriteLine("********************************");
+            Console.WriteLine("*      Willkommen zurück!      *");
+            Console.WriteLine("********************************");
+            Console.WriteLine("Drücke 'R' für kürzlich verwendete Notizen.");
+            Console.WriteLine("Drücke 'N' für eine neue Notiz.");
+
+            ConsoleKeyInfo keyInfo;
+
+            do
+            {
+                keyInfo = Console.ReadKey();
+                Console.WriteLine();
+
+                if (keyInfo.Key == ConsoleKey.R)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Kürzlich verwendete Notizen werden geladen...");
+                }
+                else if (keyInfo.Key == ConsoleKey.N)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Neue Notiz wird erstellt...");
+                }
+                else
+                {
+                    Console.WriteLine("Ungültige Eingabe. Drücke 'R' oder 'N'.");
+                }
+            } while (keyInfo.Key != ConsoleKey.R && keyInfo.Key != ConsoleKey.N);
         }
 
         static void Manager(string path)
