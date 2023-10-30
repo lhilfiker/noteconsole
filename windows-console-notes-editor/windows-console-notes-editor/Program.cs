461ecaa8ec367f8863f130d62b83b75afab326a5
Milan <m.jankovic.inf23@stud.bbbaden.ch>
2023-10-30 14:41:53 +0100

    added recent files tab

--- windows-console-notes-editor/windows-console-notes-editor/Program.cs
+++ windows-console-notes-editor/windows-console-notes-editor/Program.cs
@@ -47,10 +47,35 @@
 
                     if (keyInfo.Key == ConsoleKey.R)
                     {
-                        Console.Clear();
-                        Console.WriteLine("Kürzlich verwendete Notizen werden geladen...");
-                        Console.Clear();
+                        try
+                        {
+                            filepath = DisplayRecentFiles();
+                            if (filepath == null || filepath == "")
+                            {
+                                throw new Exception();
+                            }
+                            break;
+                        }
+                        catch
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.Clear();
 
+                            Console.WriteLine("             _                                 _      ");
+                            Console.WriteLine("            | |                               | |     ");
+                            Console.WriteLine(" _ __   ___ | |_ ___  ___ ___  _ __  ___  ___ | | ___ ");
+                            Console.WriteLine("| '_ \\ / _ \\| __/ _ \\/ __/ _ \\| '_ \\/ __|/ _ \\| |/ _ \\");
+                            Console.WriteLine("| | | | (_) | ||  __/ (_| (_) | | | \\__ \\ (_) | |  __/");
+                            Console.WriteLine("|_| |_|\\___/ \\__\\___|\\___\\___/|_| |_|___/\\___/|_|\\___|");
+                            Console.WriteLine("                                                      ");
+                            Console.WriteLine("********************************");
+                            Console.WriteLine("*      Willkommen zurück!      *");
+                            Console.WriteLine("********************************");
+                            Console.WriteLine("Drücke 'R' für kürzlich verwendete Notizen.");
+                            Console.WriteLine("Drücke 'S' um eine Datei aus deinem Computer auszuwählen");
+                            Console.WriteLine("Drücke 'N' für eine neue Notiz.");
+                        }
                     }
                     else if (keyInfo.Key == ConsoleKey.N)
                     {
@@ -79,7 +104,6 @@
                                 Console.WriteLine($"Fehler beim Erstellen der Datei: {ex.Message}");
                             }
                         }
-                        Thread.Sleep(1000);
                         filepath = filePath;
                         break;
                     }
@@ -101,7 +125,7 @@
                     }
                     else
                     {
-                        Console.WriteLine("Ungültige Eingabe. Drücke 'R' oder 'N'.");
+                        Console.WriteLine("Ungültige Eingabe. Drücke 'R', 'S' oder 'N'.");
                     }
                 } while (true);
 
@@ -109,13 +133,60 @@
             }
         }
 
-        static void Manager(string path)
+        static string DisplayRecentFiles()
         {
-            
-        }
-        static void FileRender(string content, int startrender, int x, int y)
-        {
+            Console.Clear();
+            Console.WriteLine("Kürzlich verwendete Notizen:");
 
+            string recentFilesData = GetValueForKey(cacheData, "last");
+            List<string> recentFiles = recentFilesData?.Split("|:|").ToList() ?? new List<string>();
+
+            if (recentFiles.Count == 0)
+            {
+                Console.WriteLine("Keine kürzlich verwendeten Notizen gefunden.");
+                Console.WriteLine("Drücke Enter, um zur vorherigen Ansicht zurückzukehren.");
+                while (true)
+                {
+                    var key = Console.ReadKey().Key;
+                    if (key == ConsoleKey.Enter)
+                        return null;
+                }
+            }
+
+            int selectedIndex = 0;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Kürzlich verwendete Notizen:");
+
+                for (int i = 0; i < recentFiles.Count; i++)
+                {
+                    if (i == selectedIndex)
+                        Console.Write(">> ");
+                    Console.WriteLine($"{i + 1}. {recentFiles[i]}");
+                }
+
+                Console.WriteLine("Drücke 'UpArrow' für vorherige Datei, 'DownArrow' für nächste Datei,");
+                Console.WriteLine("'Enter', um auszuwählen, oder 'Esc', um zur vorherigen Ansicht zurückzukehren.");
+
+                var key = Console.ReadKey().Key;
+                if (key == ConsoleKey.UpArrow)
+                {
+                    selectedIndex = Math.Max(0, selectedIndex - 1);
+                }
+                else if (key == ConsoleKey.DownArrow)
+                {
+                    selectedIndex = Math.Min(recentFiles.Count - 1, selectedIndex + 1);
+                }
+                else if (key == ConsoleKey.Enter)
+                {
+                    return recentFiles[selectedIndex];
+                }
+                else if (key == ConsoleKey.Escape)
+                {
+                    return null;
+                }
+            }
         }
     }
 }
