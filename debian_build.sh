9d5004a6ac38eab4d7d2246ade7bc03b6547c54c
RebelCoderJames <142885453+RebelCoderJames@users.noreply.github.com>
2023-11-16 11:21:47 +0100

    fix: dont include unneccecary files

--- debian_build.sh
+++ debian_build.sh
@@ -4,35 +4,33 @@
 echo "Enter the version number for the package (e.g., 0.1.0):"
 read version
 
-# Clone the repository
-git clone https://github.com/RebelCoderJames/noteconsole.git
-
-# Install dependencies
-sudo apt-get update && sudo apt-get install -y dotnet-sdk-6.0 dpkg-dev debhelper gpg fakeroot
+cd noteconsole
+rm -rf noteconsole/DEBIAN
+rm -rf package
 
 # Build the .Net Application
-cd noteconsole/noteconsole
-dotnet publish -c Release -r linux-x64 --self-contained # You can ignore the errors safely.
-
-# Create the Debian File Structure
-mkdir -p ../DEBIAN ../usr/share/noteconsole
-cp -r noteconsole/bin/Release/net6.0/linux-x64/publish/* ../usr/share/noteconsole/
+dotnet publish -c Release -f net6.0 --self-contained
+mkdir -p package/DEBIAN package/usr/lib/noteconsole
+cp -r noteconsole/bin/Release/net6.0/publish/* package/usr/lib/noteconsole/
+# Create Symbolic Link for Executable
+mkdir -p package/usr/bin
+ln -s /usr/lib/noteconsole/noteconsole package/usr/bin/noteconsole
 
 # Create a Debian Control File
-cat <<EOF >../DEBIAN/control
+cat <<EOF > package/DEBIAN/control
 Package: noteconsole
 Version: $version
 Section: editors
 Priority: optional
 Architecture: amd64
 Maintainer: RebelCoderJames <contact@rebelcoderjames.tech>
+Depends: dotnet-runtime-6.0
 Recommends: xsel
 Description: A simple note editor for the console.
 EOF
 
 # Build the .deb package
-cd ..
-dpkg-deb --build noteconsole
+dpkg-deb --build package
 
 # Optional: Rename the output file
-mv noteconsole.deb noteconsole_"$version"_amd64.deb
+mv package.deb noteconsole_"$version"_amd64.deb
