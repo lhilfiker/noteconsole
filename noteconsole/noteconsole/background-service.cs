ad8a3705183fa5638de49104c4f45c065157b946
RebelCoderJames <gitschol.3kcah@privacyshield.online>
2023-11-30 10:51:20 +0100

    remove testing colors

--- noteconsole/noteconsole/background-service.cs
+++ noteconsole/noteconsole/background-service.cs
@@ -9,11 +9,7 @@ namespace noteconsole
             while (true)
             {
                 ColorsListBuffer.Clear();
-                ColorsListBuffer.Add(new ColorsGlobal{line = 0, StartChar = 0, EndChar = 5, Color = ConsoleColor.Blue, BackgroundColor = ConsoleColor.Yellow});
-                ColorsListBuffer.Add(new ColorsGlobal{line = 1, StartChar = 0, EndChar = 10, Color = ConsoleColor.Green});
-                ColorsListBuffer.Add(new ColorsGlobal{line = 1, StartChar = 11, EndChar = 20, Color = ConsoleColor.Red});
-                ColorsListBuffer.Add(new ColorsGlobal{line = 2, StartChar = 0, EndChar = 50, Color = ConsoleColor.DarkCyan});
-
+                
                 GlobalColorList.Clear();
                 GlobalColorList = ColorsListBuffer.ToList();
                 
