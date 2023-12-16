9a12c9c9041a7af83a7882bc7c78837d4e43d238
RebelCoderJames <gitschol.3kcah@privacyshield.online>
2023-12-16 21:41:25 +0100

    update target framework

--- debian_build.sh
+++ debian_build.sh
@@ -9,9 +9,9 @@ rm -rf noteconsole/DEBIAN
 rm -rf package
 
 # Build the .Net Application
-dotnet publish -c Release -f net6.0 --self-contained -r linux-x64
+dotnet publish -c Release -f net8.0 --self-contained -r linux-x64
 mkdir -p package/DEBIAN package/usr/lib/noteconsole
-cp -r noteconsole/bin/Release/net6.0/linux-x64/publish/* package/usr/lib/noteconsole/
+cp -r noteconsole/bin/Release/net8.0/linux-x64/publish/* package/usr/lib/noteconsole/
 # Create Symbolic Link for Executable
 mkdir -p package/usr/bin
 ln -s /usr/lib/noteconsole/noteconsole package/usr/bin/noteconsole
